31b8c41a014ab74ec894c777983595c59364e0d9
Shubham <shubhambandkar08@gmail.com>
2025-05-05 18:20:01 +0530

    Add to cart

--- cypress/Pages/AddToCart.js
+++ cypress/Pages/AddToCart.js
@@ -6,7 +6,12 @@ export class AddToCart {
         email: '#input-email',
         Lpassword: "#input-password",
         loginButton: "aside[id='column-right'] a:nth-child(1)",
-        formLoginButton:'form > .btn'
+        formLoginButton:'form > .btn',
+        menuClick:'.nav > :nth-child(1) > .dropdown-toggle',
+        submMenuClick:'.open > .dropdown-menu > .dropdown-inner > .list-unstyled > :nth-child(2) > a',
+        AddToCartButton:"[onclick=\"cart.add('41', '1');\"] > .fa",
+        openCartPage:'#cart-total',
+        viewCart:"[href='https://naveenautomationlabs.com/opencart/index.php?route=checkout/cart'] > strong"
     }
     openURl() {
         cy.visit(Cypress.env('URL')) // Visit the URL from the environment variable 
@@ -26,6 +31,22 @@ export class AddToCart {
     clickOnloginFromButton() {
         cy.get(this.AddCartWebLocator.formLoginButton).click()
     }
+    ClickOnMenu(){
+        cy.get(this.AddCartWebLocator.menuClick).click()
+      //  cy.get(this.AddCartWebLocator.submMenuClick).click()
+    }   
 
+     
+   ClickSubMenu(){
+        cy.get(this.AddCartWebLocator.submMenuClick).click()
+    }  
+    
+    addItemToCart(){  
+        cy.get(this.AddCartWebLocator.AddToCartButton).click()
 
-}
+     }
+     opencartPage(){
+        cy.get(this.AddCartWebLocator.openCartPage).click()
+        cy.get(this.AddCartWebLocator.viewCart).click()
+     }
+    }
--- cypress/e2e/Test/addToCart.cy.js
+++ cypress/e2e/Test/addToCart.cy.js
@@ -1,30 +1,38 @@
 import { AddToCart } from '../../Pages/AddToCart';
 
 const addcartObj = new AddToCart()
-let registerData ;
-describe('test Automation', () => {
+let registerData;
+describe('test Automation', {
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+},
+  () => {
     beforeEach(() => {
-        // Load fixture correctly
-        cy.fixture('registerData').then((data) => {
-          registerData = data;
-        });
+      // Load fixture correctly
+      cy.fixture('registerData').then((data) => {
+        registerData = data;
       });
+    });
 
     it('addToCart Flow', () => {
-//cy.visit('https://naveenautomationlabs.com/opencart/index.php?route=account/login')  
-            addcartObj.openURl()
-            cy.wait(2000)
-            addcartObj.ClickOnloginButton() 
-            addcartObj.EnterEmail(registerData.email)
-            addcartObj.EnterPassword(registerData.Epassword)     
-            addcartObj.clickOnloginFromButton()
-            cy.wait(2000)   
+      //cy.visit('https://naveenautomationlabs.com/opencart/index.php?route=account/login')  
+      addcartObj.openURl()
+      cy.wait(2000)
+      addcartObj.ClickOnloginButton()
+      addcartObj.EnterEmail(registerData.email)
+      addcartObj.EnterPassword(registerData.Epassword)
+      addcartObj.clickOnloginFromButton()
+      addcartObj.ClickOnMenu()
+      addcartObj.ClickSubMenu()
+      addcartObj.addItemToCart()
+      addcartObj.opencartPage()
+      cy.wait(2000)
 
     })
 
 
 
 
-})
+  })
 
 
